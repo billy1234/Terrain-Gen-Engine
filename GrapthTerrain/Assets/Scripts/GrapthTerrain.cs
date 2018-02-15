52d91ab170c0756c7908b1e5f6d7d499566ca95b
Billy Mihalarias <bmihalarias@gmail.com>
2018-02-15 16:28:57 +1100

    added proper git ignore

---
+++ GrapthTerrain/Assets/Scripts/GrapthTerrain.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace GraphTerrain {
+
+    public class graph {
+        public List<node> nodes;
+
+        public graph(node[] nodes) {
+            this.nodes = new List<node>(nodes);
+        }
+    }
+
+    public class node {
+
+        public List<node> connnections;
+
+        public node() {
+            connnections = new List<node>();
+        }
+
+        public node(List<node> conenctions) {
+            connnections = new List<node>(connnections);
+        }
+
+        public void connect(params node[] nodes) {
+
+            for (int i = 0; i < nodes.Length; i++) {
+                connnections.Add(nodes[i]);
+            }
+        }
+    }
+}
---
+++ GrapthTerrain/Assets/Scripts/Gterrain.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GraphTerrain;
+
+public class Gterrain : MonoBehaviour {
+    public graph g;
+
+ 
+	
+	void Start () {
+
+        List<worldNode> nodes = new List<worldNode>(10);
+        for (int i = 0; i < 10; i++) {
+            nodes.Add(new worldNode(new Vector3(Random.Range(-10f, 10f), Random.Range(-10f, 10f), Random.Range(-10f, 10f))));
+            nodes[i].spawn();
+            if (i > 0) {
+                nodes[i - 1].connect(nodes[i]);
+            }
+            if (i > 1) {
+                nodes[i - 2].connect(nodes[i]);
+            }
+        }
+        g = new graph(nodes.ToArray());
+    }
+	
+	// Update is called once per frame
+	void OnDrawGizmos () {
+        if (Application.isPlaying) {
+            for (int i = 0; i < g.nodes.Count; i++) {
+                ((worldNode)g.nodes[i]).draw();
+            }
+        }
+	}
+
+    public class worldNode : node {
+        public Vector3 position;
+        GameObject worldInstance;
+
+        public worldNode(Vector3 position) : base() {
+            this.position = position;
+        }
+
+        public void spawn() {
+            if (worldInstance == null) {
+                worldInstance = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                worldInstance.transform.position = position;
+            }
+        }
+
+        public void draw() {
+            if (Application.isEditor) {
+                for (int i = 0; i < connnections.Count; i++)
+                    Debug.DrawLine(position, ((worldNode)connnections[i]).position);
+            }
+        }
+    }
+}
